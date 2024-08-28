e20eb11d312379cad89be0fd68f38a27a6479fc8
Shasha0610 <shashankmathur0610@gmail.com>
2024-08-29 00:34:00 +0530

    shasha committed

--- public/Login/login.js
+++ public/Login/login.js
@@ -51,3 +51,21 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
         submitButton.textContent = "Login";
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password');
+
+    togglePassword.addEventListener('click', function () {
+        // Toggle the password visibility
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            togglePassword.classList.remove('fa-eye');
+            togglePassword.classList.add('fa-eye-slash');
+        } else {
+            passwordInput.type = 'password';
+            togglePassword.classList.remove('fa-eye-slash');
+            togglePassword.classList.add('fa-eye');
+        }
+    });
+});
